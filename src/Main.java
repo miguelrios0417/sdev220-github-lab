06201fd7b9581a70d7bc85afaa8a0ab63c3fd891
Miguel <rioshuizar.miguel@student.greenriver.edu>
2023-06-10 12:03:39 -0700

    Added documentation to the project.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+/***
+ * This is the entry point to my sample app.
+ *
+ * @author Miguel Rios
+ * @version 1.0
+ */
+
 public class Main {
 
+    /***
+     * Starts the application
+     * @param args command-line args (not used)
+     */
     public static void main(String[] args) {
 
         System.out.println("Hello world, from SDEV 220!");
